4a0076fe8862858cc76bfa1c70d6d2bc40f4067e
Megan Arellano <ssciolist@gmail.com>
2018-02-27 09:19:51 -0700

    adds employee instantiation

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,15 @@
 class Employee
+  attr_reader :employee_id,
+              :name,
+              :role,
+              :start_date,
+              :end_date
+
+  def initialize(employee_id, name, role, start_date, end_date)
+    @employee_id = employee_id
+    @name = name
+    @role = role
+    @start_date = start_date
+    @end_date = end_date
+  end
 end
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -3,6 +3,9 @@ require './lib/employee'
 
 class EmployeeTest < Minitest::Test
   def test_instantiation
+    employee = Employee.new("1", "Susan Smith", "Manager", "2016-01-01", "2018-02-20")
+
+    assert_instance_of Employee, employee
   end
 
   def test_attributes
