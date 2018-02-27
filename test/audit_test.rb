27636ee3860274f755d2b42732f442ea11198418
Megan Arellano <ssciolist@gmail.com>
2018-02-27 11:47:29 -0700

    current progress

--- test/audit_test.rb
+++ test/audit_test.rb
@@ -16,9 +16,38 @@ class AuditTest < Minitest::Test
     assert_instance_of Company, result
   end
 
-  def test_were_invalid_days_worked
+  def test_were_invalid_employees_working
     audit = Audit.new
     company = Company.new
+    company.load_projects('./data/projects.csv')
+    company.load_employees('./data/employees.csv')
+    company.load_timesheets('./data/timesheets.csv')
+    audit.load_company(company)
+    result = audit.were_invalid_employees_working
+
+    assert_equal 3, result
+  end
+
+  def test_were_invalid_projects_being_worked_on
+    audit = Audit.new
+    company = Company.new
+    company.load_projects('./data/projects.csv')
+    company.load_employees('./data/employees.csv')
+    company.load_timesheets('./data/timesheets.csv')
+    audit.load_company(company)
+    result = audit.were_invalid_projects_being_worked_on
+
+    assert_equal false, result
+  end
+
+  def test_were_invalid_days_worked
+    skip
+    audit = Audit.new
+    company = Company.new
+    company.load_projects('./data/projects.csv')
+    company.load_employees('./data/employees.csv')
+    company.load_timesheets('./data/timesheets.csv')
+    audit.load_company(company)
     result = audit.were_invalid_days_worked
 
     assert result.include?("Invalid Days Worked:")
